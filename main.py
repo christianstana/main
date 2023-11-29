cd8a2c7aaeaff79c6e99e078848fe914c3a615cb
Christian Alonso <85254926+christianstana@users.noreply.github.com>
2023-11-29 19:16:25 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,1 @@
-def es_primo(numero):
-    if numero <= 1:
-        return False
-    for i in range(2, int(numero**0.5) + 1):
-        if numero % i == 0:
-            return False
-    return True
-
-def test_es_primo():
-    assert es_primo(2) ==
-
-if __name__ == "__main__":
-    test_es_primo()
-
+def es_primo(numero): if numero <= 1: return False for i in range(2, int(numero**0.5) + 1): if numero % i == 0: return False return True def test_es_primo(): assert es_primo(2) == True assert es_primo(4) == False assert es_primo(17) == True if __name__ == "__main__": test_es_primo()
